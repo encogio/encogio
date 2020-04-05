6f0b01cfda44781f6a897e2fff00513ade246156
Alejandro Gomez <alejandro.gomez@47deg.com>
2020-04-05 02:08:59 +0200

    :fire:

--- src/encogio/app.clj
+++ src/encogio/app.clj
@@ -10,6 +10,4 @@
   (let [port (Integer/valueOf (or (System/getenv "PORT") "8000"))
         redis-url (or (System/getenv "REDIS_URL") "127.0.0.1")
         redis-conn {:pool {} :spec {:url redis-url}}]
-    (println (str "~> Starting encog.io on port " port ))
-    (println (str "~> Pinging Redis...") (wcar redis-conn (car/ping)))
     (run-jetty http/app {:port port :join? false})))
