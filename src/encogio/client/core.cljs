f8f110f4bb87dfb4d38d6f291fb0ddaf7a560cbc
Alejandro Gomez <alejandro.gomez@47deg.com>
2020-04-07 21:07:11 +0200

    HTTP request based on promises

--- src/encogio/client/core.cljs
+++ src/encogio/client/core.cljs
@@ -3,48 +3,54 @@
    [rum.core :as rum]
    [cljsjs.clipboard]
    [goog.dom :as dom]
-   [cljs.reader :as r]
-   [goog.net.XhrIo :as xhr]))
+   [goog.events :as ev :refer [listen unlisten]]
+   [promesa.core :as p]
+   [cljs.reader :as r])
+  (:import [goog.net XhrIo EventType]))
 
 ;; io
 
-;; todo: http error handling, timeout, offline
+(def json-headers
+  (clj->js {"Accept" "application/json"
+            "Content-Type" "application/json"}))
+
 (defn shorten!
-  [state url]
-  (swap! state assoc :ongoing-request true)
-  (xhr/send "/api/shorten"
-            (fn [event]
-              (let [response (-> event .-target)
-                    status (.getStatus response)]
-                (case status
-                  200
-                  (let [body (.getResponseJson response)
-                        shortened {:url (.-url body)
-                                   :short-url (aget body "short-url")}
-                        short-urls (take 3 (conj (:short-urls @state) shortened))]
-                    (swap! state assoc
-                           :url (:url shortened)
-                           :short-urls short-urls
-                           :ongoing-request false))
-
-                  500 (do (println :server-error))
-
-                  400 (do (println :invalid-url))
-
-                  429 (do (println :rate-limit))
-
-                  (do
-                    (println :unknown-error)))))
-            "POST"
-            (js/JSON.stringify #js {:url url})
-            (clj->js {"Accept" "application/json"
-                      "Content-Type" "application/json"})))
+  [url]
+  (p/create
+   (fn [resolve reject]
+     (let [body (js/JSON.stringify #js {:url url})
+           request (XhrIo.)]
+       (listen request
+               EventType.COMPLETE
+               (fn [event]
+                 (let [response (-> event .-target)
+                       status (.getStatus response)]
+                   (case status
+                     200
+                     (let [body (.getResponseJson response)
+                           shortened {:url (.-url body)
+                                      :short-url (aget body "short-url")}]
+                       (resolve shortened))
+                     500 (reject :server-error)
+                     400 (reject :invalid-url)
+                     429 (reject :rate-limit)
+                     (reject :unknown-error)))))
+       (listen request
+               EventType.ERROR
+               (fn [err] ;; ErrorCode.{ TIMEOUT, EXCEPTION, HTTP_ERROR, ABORT }
+                 (reject :network-error)))
+       (.send request
+              "/api/shorten"
+              "POST"
+              body
+              json-headers)))))
 
 ;; ui
 
 (rum/defc url-input < rum/reactive
   [state]
-  (let [{:keys [url ongoing-request]} (rum/react state)]
+  (let [{:keys [url
+                ongoing-request]} (rum/react state)]
     [:form
      [:input {:type "text"
               :id "url"
@@ -57,9 +63,15 @@
       {:disabled (if ongoing-request "disabled" "")       
        :on-click (fn [ev]
                    (.preventDefault ev)
-                   (shorten! state url))}
-      "Encoger"
-      ]]))
+                   (swap! state assoc :ongoing-request true)
+                   (p/then (shorten! url)
+                           (fn [shortened]
+                             (let [short-urls (take 3 (conj (:short-urls @state) shortened))]
+                               (swap! state assoc
+                                      :url (:url shortened)
+                                      :short-urls short-urls
+                                      :ongoing-request false)))))}
+      "Encoger"]]))
 
 (rum/defcs copy-button
   < (rum/local false ::copied?)
