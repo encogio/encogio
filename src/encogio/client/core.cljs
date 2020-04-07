dc98c75ef87d8af2ca74ad6bcbb83a1020bef8e0
Alejandro Gomez <alejandro.gomez@47deg.com>
2020-04-07 23:44:13 +0200

    Crude but display error message

--- src/encogio/client/core.cljs
+++ src/encogio/client/core.cljs
@@ -9,6 +9,7 @@
 (rum/defc url-input < rum/reactive
   [state]
   (let [{:keys [url
+                error
                 ongoing-request]} (rum/react state)]
     [:form
      [:input {:type "text"
@@ -23,13 +24,19 @@
        :on-click (fn [ev]
                    (.preventDefault ev)
                    (swap! state assoc :ongoing-request true)
-                   (p/then (io/shorten! url)
-                           (fn [shortened]
-                             (let [short-urls (take 3 (conj (:short-urls @state) shortened))]
-                               (swap! state assoc
-                                      :url (:url shortened)
-                                      :short-urls short-urls
-                                      :ongoing-request false)))))}
+                   (-> (io/shorten! url)
+                       (p/then
+                        (fn [shortened]
+                          (let [short-urls (take 3 (conj (:short-urls @state) shortened))]
+                            (swap! state assoc
+                                   :url (:url shortened)
+                                   :error nil
+                                   :short-urls short-urls
+                                   :ongoing-request false))))
+                       (p/catch (fn [err]
+                                  (swap! state assoc
+                                         :error err
+                                         :ongoing-request false)))))}
       "Encoger"]]))
 
 (rum/defcs copy-button
@@ -73,15 +80,23 @@
       [:a {:href short-url} short-url]
       (copy-button short-url)])])
 
+(rum/defc error-message < rum/reactive
+  [state]
+  (let [{:keys [error]} (rum/react state)]
+    (when error
+      [:p (pr-str error)])))
+
 ;; app
 
 (defonce app-state
   (atom {:url ""
+         :error nil
          :short-urls []
          :ongoing-request false}))
 
 (defn mount!
   []
+  (rum/mount (error-message app-state) (dom/getElement "error-message"))
   (rum/mount (shortened-links app-state) (dom/getElement "shortened-links"))
   (rum/mount (url-input app-state) (dom/getElement "shorten-form")))
 
--- src/encogio/client/io.cljs
+++ src/encogio/client/io.cljs
@@ -1,42 +1,30 @@
 (ns encogio.client.io
   (:require
-   [goog.events :refer [listen unlisten]]
-   [promesa.core :as p])
-  (:import [goog.net XhrIo EventType]))
+   [promesa.core :as p]
+   [goog.net.XhrIo :as xhr]))
 
-;; io
+(def timeout 30000)
 
-(def json-headers
+(def headers
   (clj->js {"Accept" "application/json"
             "Content-Type" "application/json"}))
+
+(def api-url "/api/shorten")
 
 (defn shorten!
   [url]
   (p/create
    (fn [resolve reject]
      (let [body (js/JSON.stringify #js {:url url})
-           request (XhrIo.)]
-       (listen request
-               EventType.COMPLETE
-               (fn [event]
-                 (let [response (-> event .-target)
-                       status (.getStatus response)]
-                   (case status
-                     200
-                     (let [body (.getResponseJson response)
-                           shortened {:url (.-url body)
-                                      :short-url (aget body "short-url")}]
-                       (resolve shortened))
-                     500 (reject :server-error)
-                     400 (reject :invalid-url)
-                     429 (reject :rate-limit)
-                     (reject :unknown-error)))))
-       (listen request
-               EventType.ERROR
-               (fn [err] ;; ErrorCode.{ TIMEOUT, EXCEPTION, HTTP_ERROR, ABORT }
-                 (reject :network-error)))
-       (.send request
-              "/api/shorten"
-              "POST"
-              body
-              json-headers)))))
+           callback (fn [event]
+                      (let [response (-> event .-target)]
+                        (case (.getStatus response)
+                          200
+                          (let [body (.getResponseJson response)]
+                            (resolve {:url (.-url body)
+                                      :short-url (aget body "short-url")}))
+                          500 (reject :server-error)
+                          400 (reject :invalid-url)
+                          429 (reject :rate-limit)
+                          (reject :network-error))))]
+       (xhr/send api-url callback "POST" body headers timeout)))))
