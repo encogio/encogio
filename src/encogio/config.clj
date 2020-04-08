189a3ac33c4e227b3f38c92f946cca8ab412dbab
Alejandro Gomez <alejandro.gomez@47deg.com>
2020-04-08 09:38:51 +0200

    Move site settings to config

--- src/encogio/config.clj
+++ src/encogio/config.clj
@@ -1,6 +1,10 @@
 (ns encogio.config
   (:require [environ.core :refer [env]]))
 
+(def site
+  {:scheme "http"
+   :host "encog.io"})
+
 (def rate-limit
   {:limit 1000
    :limit-duration 3600})
--- src/encogio/url.clj
+++ src/encogio/url.clj
@@ -1,17 +1,17 @@
 (ns encogio.url
+  (:require [encogio.config :as config])
   (:import [java.net URL MalformedURLException]))
-
-(def site-scheme "http")
-(def site-host "encog.io")
 
 (defn validate
   [s]
   (try
     (let [parsed (URL. s)]
-      (when (not= (.getHost parsed) site-host)
+      (when (not= (.getHost parsed) (:host config/site))
         (.toString parsed)))
     (catch MalformedURLException _)))
 
 (defn urlize
-  [k]
-  (str site-scheme "://" site-host "/" k))
+  ([k]
+   (urlize config/site k))
+  ([{:keys [scheme host]} k]
+    (str scheme "://" host "/" k)))
